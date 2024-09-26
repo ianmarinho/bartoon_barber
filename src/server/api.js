85c6cc9db2e464836c9fe6c064b6ce94910cee68
ianmarinho <130616725+ianmarinho@users.noreply.github.com>
2024-09-26 15:20:35 -0300

    Delete src directory

--- src/server/api.js
+++
@@ -1,8 +0,0 @@
-// src/server/api.js ou onde você configura o Axios
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'http://localhost:5000', // ou a URL do seu backend
-});
-
-export default api;
